4494356e0f215ac17985abadba824060415f02c6
Darryl Greig <darryl.greig@owerya.com>
2023-11-02 16:55:00 +0000

    updated to check subscriptions and add if not there

--- samples/helloaws.py
+++ samples/helloaws.py
@@ -18,14 +18,26 @@ for bucket in s3.buckets.all():
 # print the buffer to stdout
 print(buffer)
 
+myEmail = "oweryaresourcing@gmail.com"
 
 # create a SNS topic (this is idempotent so will not recreate the topic if it exists)
 topic = sns.create_topic(Name='SampleTopic')
 
-# subscribe my email address to the SNS topic
-topic.subscribe(Protocol='email', Endpoint='darryl.greig@owerya.com')
+# check if the user is subscribed to the topic
+response = topic.subscriptions.all()
 
-# publish the buffer to the topic
+# Check if the user is subscribed to the topic
+found = False
+for subscription in response:
+    if subscription.attributes['Endpoint'] == myEmail:
+        found = True
+        break
+
+if not found:
+    # subscribe my email address to the SNS topic
+    topic.subscribe(Protocol='email', Endpoint=myEmail)
+
+# publish the buffer to the topic (won't work the first time if not subscribed)
 response = topic.publish(
         Message="My buckets: " + buffer
     )
